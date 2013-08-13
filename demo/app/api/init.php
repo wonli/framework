851a5f23ab3eecca9d1857b88192594325ef2772
wonli <ideaa@qq.com>
2013-08-13 17:25:06 +0800

    add api init.php comment

--- demo/app/api/init.php
+++ demo/app/api/init.php
@@ -2,46 +2,89 @@
 /**
 * app配置文件
 */
-$init = array();
 
-$init['sys'] = array(
-    'debug'=>true,
-    'auth'=>'COOKIE',
-    'default_tpl'=>'api',
-    'display'=>'JSON' //输出格式
+return array(
+
+    /**
+     * 系统设置
+     */
+    'sys' => array(
+
+        /**
+         * 登录认证保存方式
+         * 支持 COOKIE 和 SESSION
+         */
+        'auth' => 'COOKIE',
+
+        /**
+         * 默认的template路径
+         */
+        'default_tpl' => 'api',
+
+        /**
+         * response输出方式
+         * 支持 JSON HTML XML 默认是HTML
+         */
+        'display' => 'JSON'
+    ),
+
+
+    'url' => array(
+
+        /**
+         * 默认调用的控制器和方法
+         */
+        '*'	=> 'Main:index',
+
+        /**
+         * 解析url的方式
+         * 1 QUERY_STRING
+         * 2 PATH_INFO
+         */
+        'type'=>2,
+
+        /**
+         * 是否使用rewrite
+         */
+        'rewrite'=>false,
+
+        /**
+         * url请求中的连接字符
+         */
+        'dot'=>'/',
+
+        /**
+         * 请求扩展
+         */
+        'ext'=>'',
+
+        /**
+         * 索引文件名称
+         */
+        'index'=>'index.php'
+    ),
+
+    /**
+     * 控制器配置
+     */
+    "controller" => array(
+
+        /**
+         * 请求http://youdomain/hi时的配置
+         */
+        'hi'=>array(
+            /**
+             * 别名: 实际调用的控制器和方法 支持字符串和数组
+             * 如: alias = array("list"=>"index") 时
+             *
+             * 请求:http://youdomain/hi/list
+             * 实际响应的url是 http://youdomain/hi/index
+             *
+             * 也可以在控制器中用静态属性_act_alias_来指定别名[优先级低于配置]
+             */
+            'alias'=>'main:index',
+        ),
+    )
 );
-
-$init['url'] = array(
-    '*'=>'Main:index',//默认路由
-    'type'=>2,//1 query string 2 path info
-    'rewrite'=>false,//是否启用rewrite true/false;
-    'dot'=>'/',
-    'ext'=>'',
-    'index'=>'index.php'//end(explode( '/', $_SERVER["SCRIPT_NAME"]))
-);
-
-# 控制器配置
-# 例如：$init["controller"] = array (
-#  'home'=>array(
-#     'alias'=>'',
-#  ),
-#)
-# :alias 是否调用其他控制器 默认控制器alias项的配置无效 以home配置项为例:
-#       如：alias = "user"; 访问http://host/home时
-#       实际调用:user->index()
-#
-#       如：alias = "user:register"; 访问http://host/home时
-#       实际调用:user->register()方法
-#
-#       如: alias = array("list"=>"index"); 访问 http://host/home/list
-#       那么实际调用为home中的index方法
-#
-#       也可以在控制器中用静态属性_act_alias_来指定别名[优先级低于配置]
-
-$init["controller"] = array(
-
-);
-
-return $init;
 
 
