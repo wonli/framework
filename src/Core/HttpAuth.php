421886061893d6c12e2837d740772e77428e9208
wonli <wonli@live.com>
2014-12-20 11:36:37 +0800

    增强http会话管理,可以指定type为实现了HttpAuthInterface的任意对象

--- src/Core/HttpAuth.php
+++ src/Core/HttpAuth.php
@@ -10,6 +10,8 @@ namespace Cross\Core;
 
 use Cross\Auth\CookieAuth;
 use Cross\Auth\SessionAuth;
+use Cross\Exception\CoreException;
+use Cross\I\HttpAuthInterface;
 
 /**
  * @Auth: wonli <wonli@live.com>
@@ -23,23 +25,43 @@ class HttpAuth
      */
     static $obj;
 
+    /**
+     * 创建用于会话管理的对象
+     *
+     * @param string|object $type
+     * <pre>
+     *  type 默认为字符串(COOKIE)
+     *  也可以是一个实现了HttpAuthInterface接口的对象
+     * </pre>
+     * @return CookieAuth|SessionAuth|HttpAuthInterface|object
+     * @throws \Cross\Exception\CoreException
+     */
     public static function factory($type = 'COOKIE')
     {
-        $type = strtoupper($type);
-        switch ($type) {
-            case 'COOKIE' :
-                self::$obj = new CookieAuth();
-                break;
+        if (! self::$obj) {
+            if (is_string($type)) {
+                $type = strtoupper($type);
+                switch ($type) {
+                    case 'COOKIE' :
+                        self::$obj = new CookieAuth();
+                        break;
 
-            case 'SESSION' :
-                self::$obj = new SessionAuth();
-                break;
+                    case 'SESSION' :
+                        self::$obj = new SessionAuth();
+                        break;
 
-            default:
-                self::$obj = new CookieAuth();
-                break;
+                    default:
+                        self::$obj = new CookieAuth();
+                        break;
+                }
+            } elseif (is_object($type)) {
+                if ($type instanceof HttpAuthInterface) {
+                    self::$obj = $type;
+                } else {
+                    throw new CoreException('必须实现HttpAuthInterface接口');
+                }
+            }
         }
-
         return self::$obj;
     }
 }
