1913d0a08ec7b9b817ab2205daf40853c78a63f8
wonli <wonli@live.com>
2015-05-05 12:46:08 +0800

    * 完善crumb

--- src/Lib/Other/Crumb.php
+++ src/Lib/Other/Crumb.php
@@ -1,27 +1,115 @@
 <?php
+/**
+ * Cross - a micro PHP 5 framework
+ *
+ * @link        http://www.crossphp.com
+ * @license     http://www.crossphp.com/license
+ * @version     1.1.3
+ */
 namespace Cross\Lib\Other;
 
+use Cross\Exception\CoreException;
+
+/**
+ * @Auth: wonli <wonli@live.com>
+ * Class Crumb
+ * @package Cross\Lib\Other
+ */
 class Crumb
 {
-    CONST SALT = '!@c#r$!o>s<s&*';
-    static $ttl = 1800; //过期时间
+    /**
+     * 过期时间
+     *
+     * @var int
+     */
+    private $ttl = 1800;
 
-    static public function oath_hotp($data)
+    /**
+     * @var string
+     */
+    private $key = '!@c#r$!o>s<s&*';
+
+    /**
+     * @var string
+     */
+    private $algorithm = 'crc32';
+
+    /**
+     * 设置过期时间
+     *
+     * @param $ttl
+     * @return $this
+     */
+    function setTTL($ttl)
     {
-        return hash_hmac('crc32', $data, self::SALT);
+        $this->ttl = $ttl;
+        return $this;
     }
 
-    static public function make($key, $action = -1)
+    /**
+     * @param $key
+     * @return $this
+     */
+    function setKey($key)
     {
-        $i = ceil(time() / self::$ttl);
-        return substr(self::oath_hotp($i . $action . $key), -12, 10);
+        $this->key = $key;
+        return $this;
     }
 
-    static public function verify($key, $crumb, $action = -1)
+    /**
+     * 设置加密算法
+     *
+     * @param string $algorithm
+     * @return $this
+     * @throws \Cross\Exception\CoreException
+     */
+    function setAlgorithm($algorithm)
     {
-        $i = ceil(time() / self::$ttl);
-        if (substr(self::oath_hotp($i . $action . $key), -12, 10) === $crumb ||
-            substr(self::oath_hotp(($i - 1) . $action . $key), -12, 10) === $crumb
+        if(! in_array($algorithm, hash_algos())) {
+            throw new CoreException("不支持的加密算法");
+        }
+
+        $this->algorithm = $algorithm;
+        return $this;
+    }
+
+    /**
+     * 生成加密数据
+     *
+     * @param $data
+     * @return string
+     */
+    public function encrypt($data)
+    {
+        return hash_hmac($this->algorithm, $data, $this->key);
+    }
+
+    /**
+     * 生成一个字符串用于校验encrypt的值
+     *
+     * @param $key
+     * @param int $action
+     * @return string
+     */
+    public function make($key, $action = -1)
+    {
+        $i = ceil(time() / $this->ttl);
+        return substr($this->encrypt($i . $action . $key), -12, 10);
+    }
+
+    /**
+     * 用make生成的校验字符串校验encrypt是否有效
+     *
+     * @param $key
+     * @param $crumb
+     * @param int $action
+     * @return bool
+     */
+    public function verify($key, $crumb, $action = -1)
+    {
+        $i = ceil(time() / $this->ttl);
+        if (substr($this->encrypt($i . $action . $key), -12, 10) === $crumb ||
+            substr($this->encrypt(($i - 1) . $action . $key), -12, 10) === $crumb
         ) {
             return true;
         }
