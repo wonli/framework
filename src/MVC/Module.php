5faadbe7a24a2e01d52dbf8f55a5f194cbaa0a36
wonli <wonli@live.com>
2015-06-04 13:09:36 +0800

    Module的getLink()变为私有不再接受参数. 增加两个方法用于获取当前link的类型和详细配置.

--- src/MVC/Module.php
+++ src/MVC/Module.php
@@ -24,67 +24,80 @@ use Cross\Exception\CoreException;
 class Module extends FrameBase
 {
     /**
-     * 连接数据库的参数
+     * link的model类型
      *
      * @var string
      */
-    private $link_params;
+    private $link_type;
+
+    /**
+     * link的model配置
+     *
+     * @var array
+     */
+    private $link_config;
 
     /**
      * 连接配置文件名
+     * <pre>
+     * 默认为项目目录下的config/db.config.php
+     * 可以在app目录下init.php文件中通过'sys' => 'db_config'指定
+     * </pre>
      *
      * @var string
      */
     protected $db_config_file;
 
     /**
-     * 数据库连接配置
+     * module配置缓存
      *
      * @var object
      */
-    protected static $database_config;
+    protected static $module_config;
 
     /**
-     * 实例化module,并创建一个与数据库的连接
+     * 解析要连接model的参数
      *
      * @param string $params 指定要连接的数据库和配置项的key, 如mysql['db']这里的params应该为mysql:db
      */
     function __construct($params = '')
     {
         parent::__construct();
-        $this->link_params = $params;
+        $this->initModelParams($params);
     }
 
     /**
-     * 返回一个数据库操作对象实例
+     * 创建model实例,参数格式和构造函数一致
      *
-     * @param string $params
+     * @param string $params 要实例化model的参数
+     * @param array $config 解析params获得的model详细配置
      * @return \Cross\Cache\RedisCache|\Cross\DB\Drivers\CouchDriver|\Cross\DB\Drivers\MongoDriver|\Cross\DB\Drivers\PDOSqlDriver
      * @throws CoreException
      */
-    function getLink($params = '')
+    function getModel($params = '', &$config = array())
     {
-        $db_config = $this->databaseConfig();
-        if ($params) {
-            list($link_type, $link_config) = explode(':', $params);
-            $link_params = $db_config->get($link_type, $link_config);
+        $config = $this->parseModelParams($params);
+        return DBFactory::make($config['model_type'], $config['model_config'], $this->config);
+    }
 
-            if (empty($link_params)) {
-                throw new CoreException("未配置的数据库: {$link_type}:{$link_config}");
-            }
-        } else {
-            if ($default = $db_config->get('default')) {
-                $default_config = each($default);
-                $link_type = $default_config['key'];
-                $link_params = $db_config->get($link_type, $default_config['value']);
-            } else if ($link_params = $db_config->get('mysql', 'db')) {
-                $link_type = 'mysql';
-            } else {
-                throw new CoreException('未找到数据库默认配置');
-            }
-        }
+    /**
+     * 当前link的model类型
+     *
+     * @return string
+     */
+    function getLinkType()
+    {
+        return $this->link_type;
+    }
 
-        return DBFactory::make($link_type, $link_params, $this->config);
+    /**
+     * 当前link的model详细配置信息
+     *
+     * @return array
+     */
+    function getLinkConfig()
+    {
+        return $this->link_config;
     }
 
     /**
@@ -94,16 +107,16 @@ class Module extends FrameBase
      */
     protected function databaseConfig()
     {
-        if (!self::$database_config) {
-            $link_config_file = $this->getDatabaseConfigFile();
-            self::$database_config = CrossArray::init(Loader::read("::config/{$link_config_file}"));
+        if (!self::$module_config) {
+            $link_config_file = $this->getModuleConfigFile();
+            self::$module_config = CrossArray::init(Loader::read("::config/{$link_config_file}"));
         }
 
-        return self::$database_config;
+        return self::$module_config;
     }
 
     /**
-     * 设置连接配置文件名
+     * 设置配置文件名
      *
      * @param $link_config_file
      */
@@ -113,11 +126,69 @@ class Module extends FrameBase
     }
 
     /**
+     * 解析指定model的类型和参数
+     *
+     * @param string $params
+     * @return array
+     * @throws CoreException
+     */
+    protected function parseModelParams($params = '')
+    {
+        $all_db_config = $this->databaseConfig();
+        if ($params) {
+            list($model_type, $model_config_key) = explode(':', $params);
+            $model_config = $all_db_config->get($model_type, $model_config_key);
+
+            if (empty($model_config)) {
+                throw new CoreException("未配置的数据库: {$model_type}:{$model_config_key}");
+            }
+        } else {
+            if ($default = $all_db_config->get('default')) {
+                $default_config = each($default);
+                $model_type = $default_config['key'];
+                $model_config = $all_db_config->get($model_type, $default_config['value']);
+            } else {
+                if ($model_config = $all_db_config->get('mysql', 'db')) {
+                    $model_type = 'mysql';
+                } else {
+                    throw new CoreException('未找到数据库默认配置');
+                }
+            }
+        }
+
+        return array('model_type' => $model_type, 'model_config' => $model_config);
+    }
+
+    /**
+     * 获取默认model的实例
+     *
+     * @return \Cross\Cache\RedisCache|\Cross\DB\Drivers\CouchDriver|\Cross\DB\Drivers\MongoDriver|\Cross\DB\Drivers\PDOSqlDriver|mixed
+     * @throws CoreException
+     */
+    private function getLink()
+    {
+        return DBFactory::make($this->link_type, $this->link_config, $this->config);
+    }
+
+    /**
+     * 初始化model_type和model_config
+     *
+     * @param string $params
+     * @throws CoreException
+     */
+    private function initModelParams($params = '')
+    {
+        $config = $this->parseModelParams($params);
+        $this->link_type = $config['model_type'];
+        $this->link_config = $config['model_config'];
+    }
+
+    /**
      * 获取连接配置文件名
      *
      * @return mixed
      */
-    private function getDatabaseConfigFile()
+    private function getModuleConfigFile()
     {
         if (!$this->db_config_file) {
             $db_config_file = parent::getConfig()->get('sys', 'db_config');
@@ -132,7 +203,7 @@ class Module extends FrameBase
     }
 
     /**
-     * 访问link属性时才与数据库建立连接
+     * 访问link属性时才实例化model
      *
      * @param $property
      * @return \Cross\Cache\RedisCache|\Cross\Core\Request|\Cross\Core\Response|\Cross\DB\Drivers\CouchDriver|\Cross\DB\Drivers\MongoDriver|\Cross\DB\Drivers\PDOSqlDriver|View|null
@@ -142,7 +213,7 @@ class Module extends FrameBase
     {
         switch ($property) {
             case 'link' :
-                return $this->link = $this->getLink($this->link_params);
+                return $this->link = $this->getLink();
 
             default :
                 return parent::__get($property);
