09045fcc8cf7b699568b49cc63f892c609aa57c7
ideaa <ideaa@qq.com>
2020-09-03 14:17:24 +0800

    LogicStatusException
    抛出逻辑状态异常

---
+++ src/Exception/LogicStatusException.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Cross - a micro PHP framework
+ *
+ * @link        http://www.crossphp.com
+ * @license     MIT License
+ */
+
+namespace Cross\Exception;
+
+
+use Cross\Core\Delegate;
+use Cross\Core\Loader;
+use Throwable;
+
+/**
+ * 逻辑状态异常
+ *
+ * @author wonli <wonli@live.com>
+ * Class LogicStatusException
+ * @package Cross\Exception
+ */
+class LogicStatusException extends CrossException
+{
+    protected $httpStatusCode = 200;
+
+    /**
+     * LogicStatusException constructor.
+     *
+     * @param int|null $code
+     * @param string|null $msg
+     */
+    function __construct(int $code = null, string $msg = null)
+    {
+        if (1 !== $code) {
+            try {
+                if (null === $msg) {
+                    $statusConfigFile = Delegate::env('sys.status') ?? 'status.config.php';
+                    if (!file_exists($statusConfigFile) && defined('PROJECT_REAL_PATH')) {
+                        $statusConfigFile = PROJECT_REAL_PATH . 'config' . DIRECTORY_SEPARATOR . $statusConfigFile;
+                    }
+
+                    $statusMsg = Loader::read($statusConfigFile);
+                    $message = $statusMsg[$code] ?? $code;
+                } else {
+                    $message = $msg;
+                }
+
+                parent::__construct($message, $code);
+            } catch (Throwable $e) {
+                parent::__construct($e->getMessage(), 0);
+            }
+        }
+    }
+}
